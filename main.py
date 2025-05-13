163b4267f35a04f5d0ff92aa11ca5ce64d50ffd0
petbell <35951575+petbell@users.noreply.github.com>
2025-05-13 14:48:42 +0100

    Dockerfile and compose tested and working perfectly. Next is to scale the app

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from fastapi import FastAPI
-from typing import Union
+from fastapi import FastAPI, Depends
+from typing import Union, Annotated
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+
+class Orders(SQLModel, table=True):
+    id: int| None = Field(default=None, primary_key=True)
+    product_id: str = Field(index=True)
+    quantity: int = Field(default=1)
+
+
+DATABASE_URL = "postgresql://postgres:postgres@db/order_db"
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+sessionDep = Annotated[Session, Depends(get_session)]
+create_db_and_tables()
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -10,5 +32,22 @@ async def root():
 def read_items(itemid: int, q: Union [str, None] =  None):
     return {"item_id": itemid, "Q": q }
         
-        
-    
+@app.get("/orders/{order_id}")
+async def get_an_order(order_id: int, session: sessionDep):
+    statement = select(Orders).where(Orders.id == order_id)
+    results = session.exec(statement)
+    order = results.first()
+    if order:
+        return order
+    else:
+        return {"message": "Order not found"}        
+
+@app.get('/orders')
+async def get_orders(session: sessionDep):
+    statement = select(Orders)
+    results = session.exec(statement)
+    orders = results.all()
+    if orders:
+        return orders
+    else:
+        return{"message": "Orders not found"}
